2e85c9de8d07cbef4be5942fee3237ee39def0d8
Jackie Chen <ecuojackie@yahoo.com>
2023-04-04 21:17:48 -0700

    Add post pagination on home page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,13 +3,14 @@ import { GET_POSTS } from "@/graphql/queries/getPosts";
 import client from "../../apollo-client";
 import VideoGrid from "@/components/Home/VideoGrid";
 import { IPost } from "../../../types/types";
+import PaginationBar from "@/components/Home/PaginationBar";
 
 interface Props {
 	posts: IPost[];
 }
 
 const Home: FunctionComponent<Props> = ({ posts }) => {
-	const [isSSR, setIsSSR] = useState(true);
+	const [isSSR, setIsSSR] = useState<boolean>(true);
 	useEffect(() => {
 		setIsSSR(false);
 	}, []);
@@ -18,6 +19,7 @@ const Home: FunctionComponent<Props> = ({ posts }) => {
 			{!isSSR && (
 				<div className="h-screen w-screen flex flex-col items-center justify-start space-y-10 font-mono pt-40 overflow-y-scroll">
 					<VideoGrid posts={posts} />
+					<PaginationBar />
 				</div>
 			)}{" "}
 		</>
@@ -30,8 +32,15 @@ const Home: FunctionComponent<Props> = ({ posts }) => {
 	);
 };
 
-export async function getServerSideProps() {
-	const { data } = await client.query({ query: GET_POSTS });
+export async function getServerSideProps(context: any) {
+	// get page number
+	const page = context.query.page ? context.query.page : 1;
+	const { data } = await client.query({
+		query: GET_POSTS,
+		variables: {
+			page: parseInt(page),
+		},
+	});
 	return {
 		props: { posts: data.posts },
 	};
