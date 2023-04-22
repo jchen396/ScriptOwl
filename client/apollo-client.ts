6ad98b37753c6f268bb5dc20bb0856fd48f44137
Jackie Chen <ecuojackie@yahoo.com>
2023-04-21 20:23:37 -0700

    Fix dynamic environment variable for server in apollo client

--- client/apollo-client.ts
+++ client/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-	uri: "http://localhost:5000/graphql",
+	uri: `${process.env.NEXT_PUBLIC_SERVER_DOMAIN}graphql`,
 	cache: new InMemoryCache(),
 	credentials: "include",
 });
