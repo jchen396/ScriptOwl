5d7c84bffe7762bb4a738f25f9e76b58f9d51b4a
Jackie Chen <ecuojackie@yahoo.com>
2023-11-06 20:27:04 -0800

    Create user profile template

--- client/src/pages/contact/index.tsx
+++ client/src/pages/contact/index.tsx
@@ -85,20 +85,19 @@ const Contact = (props: Props) => {
 				onSubmit={(e) => handleFormSubmit(e)}
 			>
 				<div className="relative">
-					<input
-						type="text"
-						id="email"
-						className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-						placeholder=" "
-						onChange={(e) => setEmail(e.target.value)}
-						required
-					/>
 					<label
 						htmlFor="email"
 						className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-black px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
 					>
 						E-mail
 					</label>
+					<input
+						type="email"
+						id="email"
+						className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+						onChange={(e) => setEmail(e.target.value)}
+						required
+					/>
 				</div>
 				<div className="relative">
 					<input
