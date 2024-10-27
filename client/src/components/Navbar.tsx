a33a262d1b05e29261e750f61993958fc95e551d
Jackie Chen <ecuojackie@yahoo.com>
2024-10-26 22:11:38 -0700

    Add watch history page template to account info

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -210,6 +210,18 @@ const Navbar: FunctionComponent<Props> = ({ userData }) => {
 									Liked Videos
 								</Link>
 							</li>
+							<li className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 hover:cursor-pointer rounded">
+								<Link
+									href={{
+										pathname: "/history",
+										query: {
+											postIds: userData?.likedPostsIds,
+										},
+									}}
+								>
+									Watch History
+								</Link>
+							</li>
 							<li
 								className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 hover:cursor-pointer rounded"
 								onClick={onSignOut}
