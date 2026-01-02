f78346947269902f46e6ec6b977696e67d2369d6
jchen396 <50890393+jchen396@users.noreply.github.com>
2026-01-01 23:42:05 -0800

    Minor change to packages for maintaining project

--- server/config/db.ts
+++ server/config/db.ts
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-	await mongoose.connect(process.env.MONGO_URI);
-	console.log(`MongoDB Connected.`);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected.`);
 };
 
 export { connectDB };
