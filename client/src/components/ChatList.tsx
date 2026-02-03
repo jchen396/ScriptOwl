e7e008ecb70a750cf49645fc1d37b4363127eb0a
jchen396 <50890393+jchen396@users.noreply.github.com>
2026-02-02 23:38:37 -0800

    Create chat list interface template in Layout

---
+++ client/src/components/ChatList.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import React from "react";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import Link from "next/link";
+
+const ChatList = () => {
+    return <div className="h-20 border-top border-t-2 border-whiet"></div>;
+};
+
+export default ChatList;
--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -3,29 +3,51 @@ import Navbar from "@/components/Navbar";
 import { useDispatch, useSelector } from "react-redux";
 import { authenticate } from "@/redux/apiCalls";
 import { IUser } from "../../../types/types";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import Footer from "./Footer";
+import ChatList from "./ChatList";
 
 interface LayoutProps {}
 
 const Layout = ({ children }: PropsWithChildren) => {
-	const { currentUser } = useSelector((state: any) => state.user);
-	const dispatch = useDispatch();
-	const [userData, setUserData] = useState<IUser>();
-	useEffect(() => {
-		authenticate(dispatch);
-	}, [dispatch]);
-	useEffect(() => {
-		setUserData(currentUser);
-	}, [currentUser]);
-	return (
-		<>
-			<div className="bg-black min-w-full min-h-screen flex flex-col">
-				<Navbar userData={userData} />
-				<main className="flex-1">{children}</main>
-				<Footer />
-			</div>
-		</>
-	);
+    const { currentUser } = useSelector((state: any) => state.user);
+    const dispatch = useDispatch();
+    const [userData, setUserData] = useState<IUser>();
+    const [showChatList, setShowChatList] = useState<boolean>(false);
+    useEffect(() => {
+        authenticate(dispatch);
+    }, [dispatch]);
+    useEffect(() => {
+        setUserData(currentUser);
+    }, [currentUser]);
+    return (
+        <>
+            <div className="bg-black min-w-full min-h-screen flex flex-col">
+                <Navbar userData={userData} />
+                <main className="flex-1">{children}</main>
+                <div className="fixed bottom-2 right-0 w-full bg-black">
+                    <div
+                        onClick={() =>
+                            showChatList
+                                ? setShowChatList(false)
+                                : setShowChatList(true)
+                        }
+                        className="flex w-full hover:text-green-400 hover:cursor-pointer items-center justify-center"
+                    >
+                        {showChatList ? (
+                            <ArrowDownwardIcon className="w-10 h-10 text-white bg-transparent" />
+                        ) : (
+                            <ArrowUpwardIcon className="w-10 h-10 text-white bg-transparent" />
+                        )}
+                        <p className="text-white">Show chat list</p>
+                    </div>
+                    {showChatList ? <ChatList /> : <></>}
+                </div>
+                <Footer />
+            </div>
+        </>
+    );
 };
 
 export default Layout;
