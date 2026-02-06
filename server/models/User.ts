4136f3f7b7b0dc1d0db41f14a239641e3561b7c6
jchen396 <50890393+jchen396@users.noreply.github.com>
2026-02-06 11:52:33 -0800

    Fix friend list now shows mutuals in ID format

--- server/models/User.ts
+++ server/models/User.ts
@@ -5,26 +5,27 @@ import { IUser } from "./../../types/types";
 
 // 2. Create a Schema corresponding to the document interface.
 const userSchema = new Schema<IUser>(
-	{
-		username: { type: String, required: true, unique: true },
-		password: { type: String, require: true },
-		email: { type: String, required: true, unique: true },
-		points: { type: Number },
-		followers: { type: [String] },
-		following: { type: [String] },
-		avatarKey: { type: String },
-		likedCommentsIds: { type: [String] },
-		dislikedCommentsIds: { type: [String] },
-		likedPostsIds: { type: [String] },
-		dislikedPostsIds: { type: [String] },
-		uploadedPostIds: { type: [String] },
-		watchHistory: { type: [Object] },
-		verificationCode: { type: Number },
-		isVerified: { type: Boolean },
-	},
-	{
-		timestamps: true,
-	}
+    {
+        username: { type: String, required: true, unique: true },
+        password: { type: String, require: true },
+        email: { type: String, required: true, unique: true },
+        points: { type: Number },
+        followers: { type: [String] },
+        following: { type: [String] },
+        friends: { type: [String] },
+        avatarKey: { type: String },
+        likedCommentsIds: { type: [String] },
+        dislikedCommentsIds: { type: [String] },
+        likedPostsIds: { type: [String] },
+        dislikedPostsIds: { type: [String] },
+        uploadedPostIds: { type: [String] },
+        watchHistory: { type: [Object] },
+        verificationCode: { type: Number },
+        isVerified: { type: Boolean },
+    },
+    {
+        timestamps: true,
+    },
 );
 
 // 3. Create a Model.
